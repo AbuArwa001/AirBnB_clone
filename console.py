e76a28256798698855194de572144fa1561d800f
AbuArwa001 <khalfanathman12@yahoo.com>
2024-02-07 02:51:56 +0300

    Write a console.py that contains the entry point of the command interpreter

--- console.py
+++ console.py
@@ -5,7 +5,22 @@ Module that imports  cmd module:
 """
 
 
-class BaseModel(cmd.Cmd):
+class HBNBCommand(cmd.Cmd):
+    """
+    console Command class that inherits
+        cmd class
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quits the cmd interface usin quit command"""
+        return True
+
+    def do_EOF(self, line):
+        """Exits command using key interrupt"""
+        return True
     pass
 
 
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
